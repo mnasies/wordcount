e190074886dc68a5ff9d7f50432c746c7809bc73
mnasie <manassehsamuel642@gmail.com>
2026-07-17 19:47:20 +0300

    Normalize word counts by removing punctuation and casing

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for line in &file_data_vec {
         for word in line.split_whitespace() {
-            *file_data.entry(word.to_string()).or_insert(0) += 1;
+            let temp_word = word.to_lowercase().to_string();
+            let mut real_word = String::new();
+            let all_characters: [char; 26] = [
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            ];
+            for ch_temp in temp_word.chars() {
+                if all_characters.contains(&ch_temp) {
+                    real_word.push(ch_temp);
+                }
+            }
+            *file_data.entry(real_word).or_insert(0) += 1;
         }
     }
 
